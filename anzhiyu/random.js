ab72a4cda7a9f1a11e1ba361a31d9bfcfca565a2
MingQing97 <wujiaxiong434@gmail.com>
2025-02-22 18:42:44 +0800

    Site updated: 2025-02-22 18:42:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/22/hello-world/","2025/02/22/这是一篇新的文章/"];function toRandomPost(){
+var posts=["2025/02/22/这是另外一篇新的博文/","2025/02/22/这是一篇新的文章/","2025/02/22/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
